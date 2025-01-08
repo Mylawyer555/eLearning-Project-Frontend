70c5ccd90ca51040c1cefd5237b5ef9b39822074
Mylawyer555 <samuelabel636@gmail.com>
2025-01-08 15:17:39 +0100

    Button components and navbar component created

---
+++ constants/Constant.tsx
@@ -0,0 +1,26 @@
+export const navlinks = [
+    {
+        id: 1,
+        url: "#",
+        title: "Home",
+        active: true
+    },
+    {
+        id: 2,
+        url: "#",
+        title: "Courses",
+        active: false
+    },
+    {
+        id: 3,
+        url: "#",
+        title: "Instructors",
+        active: false
+    },
+    {
+        id: 4,
+        url: "#",
+        title: "About",
+        active: false
+    }
+]
